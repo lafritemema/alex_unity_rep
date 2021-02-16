13785308969cf1c277d6f46985cd285bbeb7d8f8
LeMac <57087362+Gendw@users.noreply.github.com>
2021-02-16 15:38:36 +0100

    Add files via upload

---
+++ floorNwall.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class floorNwall : MonoBehaviour
+{
+    public GameObject plane;
+    protected float Xnumber;
+    protected float Znumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+      /*  if (File.Exists(Application.dataPath + "/streamingAssets/jsontest.json"))
+        {
+            string readingstring = File.ReadAllText(Application.dataPath + "/streamingAssets/jsontest.json");
+            keyboardScript.SaveObject sonmIIsonmII = JsonUtility.FromJson<keyboardScript.SaveObject>(readingstring);
+
+             UnityEngine.Debug.Log("Test sonmIIsonmII");
+             UnityEngine.Debug.Log(sonmIIsonmII.x);
+             UnityEngine.Debug.Log(sonmIIsonmII.z);
+
+            Xnumber = float.Parse(sonmIIsonmII.x);
+              Znumber = float.Parse(sonmIIsonmII.z);
+
+
+              UnityEngine.Debug.Log("Test sonmIIsonmII apres le parsing");
+              UnityEngine.Debug.Log(Xnumber);
+              UnityEngine.Debug.Log(Znumber);
+            
+             Xnumber = sonmIIsonmII.x;
+             Znumber = sonmIIsonmII.z;
+            
+            
+        }*/
+        UnityEngine.Debug.Log("floorNwall X=" + keyboardScript.x);
+        UnityEngine.Debug.Log("floorNwall Z=" + keyboardScript.z);
+        Xnumber = keyboardScript.x;
+        Znumber = keyboardScript.z;
+
+        plane.transform.localScale = new Vector3(Xnumber, 1, Znumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
---
+++ loadGame.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadGame : MonoBehaviour
+{
+    public GameObject canvasXYZ;
+    public GameObject canvasMenu;
+    public GameObject xText;
+    public GameObject zText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void load()
+    {
+        /*  if (PlayerPrefs.HasKey("scene"))
+          {
+              UnityEngine.Debug.Log("Chargement en cours");
+              SceneManager.LoadScene(PlayerPrefs.GetString("scene"));
+          }*/
+        Application.LoadLevel("floorNwall");
+
+    }
+
+    public void testScene()
+    {
+        
+        Application.LoadLevel("RightScene");
+
+    }
+
+    public void newGame()
+    {
+        canvasMenu.SetActive(false);
+        canvasXYZ.SetActive(true);
+    }
+
+    public void X()
+    {
+        xText.SetActive(true);
+        zText.SetActive(false);
+    }
+
+    public void Z()
+    {
+        xText.SetActive(false);
+        zText.SetActive(true);
+    }
+
+    public void retour()
+    {
+        canvasMenu.SetActive(true);
+        canvasXYZ.SetActive(false);
+    }
+}
